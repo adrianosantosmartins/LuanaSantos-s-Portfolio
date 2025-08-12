58622950d0ca371ceb42eb35b06da13388ce21f5
adrianosantosmartins <adriano.santosmartins1812@gmail.com>
2025-08-12 19:32:52 -0300

    up

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ const observer = new IntersectionObserver((entries) => {
 elements.forEach(el => {
   observer.observe(el);
 });
+function lips(){
+  const overlay = document.getElementById('overlay');
+  const container = document.getElementById('imageContainer');
+  overlay.style.pointerEvents = 'auto';
+  overlay.style.opacity = 1;
+  const images = [
+        'pictures/lips/goku.png',
+        'pictures/lips/goku1.png',        
+        'pictures/lips/goku2.png',
+        'pictures/lips/goku3.png'
+      ];
+ images.forEach((src, index) => {
+        const img = document.createElement('img');
+        img.src = src;
+        container.appendChild(img);
+
+        img.onload = () => {
+          setTimeout(() => {
+            img.style.opacity = 1;
+          }, index * 500); // delay de 500ms entre cada imagem
+        };
+      });
+
+      // Fechar overlay ao clicar nele, limpando as imagens
+      overlay.addEventListener('click', () => {
+        overlay.style.opacity = 0;
+        overlay.style.pointerEvents = 'none';
+        container.innerHTML = '';
+      });
+}
