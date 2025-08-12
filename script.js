3fc94e29f1392252443366837e15056b8074cfb7
adrianosantosmartins <adriano.santosmartins1812@gmail.com>
2025-08-12 11:38:30 -0300

    update

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function menu() {
        itens.style.display = 'block';
    }
 }
+const elements = document.querySelectorAll('.fade-in');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.add('show');
+        },300);
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.1});
+elements.forEach(el => {
+  observer.observe(el);
+});
